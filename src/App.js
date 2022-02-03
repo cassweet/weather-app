a3a8908a5a54812c4ca4cac61a729b8d5d4ceb38
cassweet <98839549+cassweet@users.noreply.github.com>
2022-02-03 13:36:55 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import Weather from './components/weather';
+import axios from 'axios'
 export default function App() {
-  
+        
+  const cities = [{
+    name: 'OTTAWA',
+    latitude: '45.4247',
+    lognitude: '-75.6950'
+  }, {
+    name: 'MONTREAL',
+    latitude: '45.5017',
+    lognitude: '-73.5673'
+  }];
+
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
 
+  const handleCurrentCity = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+  }
+
+  const handleClick = (index) => {
+    setLat(cities[index].latitude);
+    setLong(cities[index].lognitude);
+  }
+
   useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
-
       await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
       .then(res => res.json())
       .then(result => {
@@ -24,14 +42,31 @@ export default function App() {
     fetchData();
   }, [lat,long])
   
+  
+
   return (
-    <div className="App">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div></div>
+    <>
+      <div style={{alignitems: "center", backgroundColor:"#ebf3fb", fontfamily: "Trebuchet MS", fontWeight: "bold", justifyContent: "space-between", whitespace: "pre-wrap"}}>
+      <b id="currentCity" onClick={handleCurrentCity}>
+        CURRENT CITY{'\t'}
+      </b>
+
+      {cities.map((city, index) =>
+        <b id="altCity" key={index} onClick={() => handleClick(index)}>
+          {city.name}{'\t'}
+        </b>
       )}
-      
     </div>
+
+      <div className="App" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        {(typeof data.main != 'undefined') ? (
+          <Weather weatherData={data}/>
+        ): (
+          <div></div>
+        )}
+
+      </div>
+      
+    </>
   );
 }
