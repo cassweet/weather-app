a32113588ad0e72f511c8dc68a55e1eed8b66da8
cassweet <98839549+cassweet@users.noreply.github.com>
2022-02-03 13:37:11 -0500

    Add files via upload

---
+++ src/components/IconWeatherComponent.jsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloud } from '@fortawesome/free-solid-svg-icons'
+import { faRain } from '@fortawesome/free-solid-svg-icons'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMist } from '@fortawesome/free-solid-svg-icons'
+import { faClear } from '@fortawesome/free-solid-svg-icons'
+import { faSnow } from '@fortawesome/free-solid-svg-icons'
+
+const icons =   {
+            Thunderstorm: "faThunderstorm",
+            Drizzle: "faRain",
+            Rain: "faRain",
+            Snow: "faSnow", 
+            Atmosphere: "wi-fog",
+            Clear: "faSun",
+            Clouds: "faCloud"
+        } 
+
+function IconWeatherComponent({icon}) {
+	
+	let [svg, setSvg] = useState('');
+		
+	useEffect(() => {
+		const setIcon = () => {
+			if (icons === '01d') {
+				setSvg('{Clear}')
+			}
+			if (icons === '02d') {
+				setSvg('{Clouds}')
+			}
+			if (icons === '02n') {
+				setSvg('{Clouds}')
+			}
+			if (icons === '03d' || icons === '03n' || icons === '04d' || icons === '04n') {
+				setSvg('{Clouds}')
+			}
+			if (icons === '09d' || icons === '09n') {
+				setSvg('{Rain}')
+			}
+			if (icons === '10d' || icons === '10n') {
+				setSvg('{Rain}')
+			}
+			if (icons === '11d' || icons === '11n') {
+				setSvg('{Thunderstorm}')
+			}
+			if (icons === '13d' || icons === '13n') {
+				setSvg('{Snow}')
+			}
+			if (icons === '50d' || icons === '50n') {
+				setSvg('{Clouds}')
+			}
+		}
+		setIcon();
+	}, [icons])
+	
+	
+	return (
+		<div>
+			<img src={'${icons}'} width="110" height="110" alt=""/>
+		</div>
+	);
+}
+
+export default IconWeatherComponent
--- src/components/weather.js
+++ src/components/weather.js
@@ -2,24 +2,22 @@ import React from 'react';
 import './styles.css';
 import { Card } from 'semantic-ui-react';
 import moment from 'moment';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloud } from '@fortawesome/free-solid-svg-icons'
-import { faRain } from '@fortawesome/free-solid-svg-icons'
-import { faSun } from '@fortawesome/free-solid-svg-icons'
-import { faMist } from '@fortawesome/free-solid-svg-icons'
-import { faClear } from '@fortawesome/free-solid-svg-icons'
-
+import { useState } from 'react';
+import { Component } from 'react';
+import IconWeatherComponent from "./IconWeatherComponent";
 
 const CardExampleCard = ({weatherData}) => (
   <Card id="card">
     <Card.Content>
         <Card.Header className="header">{weatherData.name}</Card.Header>
         <p className="day">{moment().format('dddd')}</p>
-        <p class="temp">{weatherData.main.temp} &deg;C</p>
-        <p class="description">{weatherData.weather[0].main}</p>
+        <p className="temp">{weatherData.main.temp} &deg;C</p>
+        <p className="description">{weatherData.weather[0].main}</p>
+         <IconWeatherComponent icons={weatherData.icons}/>
     </Card.Content>
-  </Card> 
+  </Card>
 )
+
 
 export default CardExampleCard;
 
